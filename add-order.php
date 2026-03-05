8bf8edb0103b94c5ab220c2b45ef2575cdb5501d
HeloOimM <helooim140@gmail.com>
2026-03-05 18:57:18 +0700

    Update add-order.php

--- add-order.php
+++ add-order.php
@@ -6,16 +6,17 @@ $price = $_POST['price'];
 $amount = $_POST['amount'];
 $unique_number = $_POST['unique_number'];
 $image = $_POST['image'];
-$sql = "insert into `order`.emp5 values('','$userorder','$item','$unique_number','$amount','$price', '$image')";
+$sql = "insert into emp5 values('','$userorder','$item','$unique_number','$amount','$price', '$image')";
 $query = mysqli_query($connect2, $sql);
 if ($query) {
 echo "<script>
 alert('Item Created!');
-location.href = 'home.php';
+location.href = 'index.php';
 </script>";
 }
 else {
 echo "<script>alert('Failed');
 location.href = 'addorder.php'</script>";
 }
+
 ?>
