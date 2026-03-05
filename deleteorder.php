46bba1621bfebf7526cd487182448c0564700a1c
HeloOimM <helooim140@gmail.com>
2026-03-05 19:01:43 +0700

    Update deleteorder.php

--- deleteorder.php
+++ deleteorder.php
@@ -1,15 +1,16 @@
 <?php include "connectorder.php";
 $id = $_GET['id'];
-$sql = "delete from order.emp5 where id = $id";
+$sql = "delete from emp5 where id = $id";
 $query = mysqli_query($connect2, $sql);
 if ($query) {
 echo "<script>
 alert('Item Deleted');
-location.href = 'home.php';
+location.href = 'index.php';
 </script>";
 }
 else {
 echo "<script>alert('Failed');
 location.href = 'home.php'</script>";
 }
+
 ?>
