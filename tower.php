60f5f92f48a63df64da334133b96abe055ad33e0
HeloOimM <helooim140@gmail.com>
2026-03-05 17:42:58 +0700

    Delete unused file

--- tower.php
+++ tower.php
@@ -1,17 +1,1 @@
-<?php include "connect.php";
-$tower = $_POST['obby'];
-$difficulty = $_POST['difficulty'];
-$uniqueID = $_POST['unique_id'];
-$sql = "insert into cruds.emp5 values('','$tower','$difficulty','$uniqueID')";
-$query = mysqli_query($connect, $sql);
-if ($query) {
-echo "<script>
-alert('Obby Created');
-location.href = 'crudtable.php';
-</script>";
-}
-else {
-echo "<script>alert('Failed');
-location.href = 'crudtable.php'</script>";
-}
-?>
+
