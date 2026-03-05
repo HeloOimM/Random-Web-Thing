0e3ec642326f937c6e0c309d20a2ef6d3843b861
HeloOimM <helooim140@gmail.com>
2026-03-05 18:59:40 +0700

    Update connectuser.php

--- connectuser.php
+++ connectuser.php
@@ -1,7 +1,8 @@
 <?php 
-$connect1 = mysqli_connect("localhost","root","","user");
+$connect1 = mysqli_connect("sql100.iceiy.com","icei_41311730","(Vpanel Password)","icei_41311730_user");
 
 if ($connect1 == false ) {
 die("Could not link: ".mysqli_connect_error());
 }
+
 ?>
