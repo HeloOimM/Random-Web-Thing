321f2b0e81090c073d8c7b90fde5818efb5bd4d9
HeloOimM <helooim140@gmail.com>
2026-03-05 18:59:15 +0700

    Update connectorder.php

--- connectorder.php
+++ connectorder.php
@@ -1,8 +1,9 @@
 <?php 
-$connect1 = mysqli_connect("sql100.iceiy.com","icei_41311730","(VPanel Password)","icei_41311730_order");
+$connect2 = mysqli_connect("sql100.iceiy.com","icei_41311730","(VPanel Password)","icei_41311730_order");
 
 if ($connect2 == false ) {
 die("Could not link: ".mysqli_connect_error());
 }
 
 ?>
+
